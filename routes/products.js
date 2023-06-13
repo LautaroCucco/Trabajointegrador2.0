66ef0fda9bb6679fe374545353f925be985b1065
LautaroCucco <lcucco@udesa.edu.ar>
2023-06-13 19:32:39 -0300

    Pusheo models

--- routes/products.js
+++ routes/products.js
@@ -1,46 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productControllers');
+const productController = require('../controllers/productController');
 
-// Configuracion de Multer para subir foto del producto
-let multer = require('multer');
-let path = require('path');
+router.get('/id/:id', productController.show);
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/images/products')) 
-    },
-    filename : function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
+router.get('/add', productController.add);
+router.post('/add', productController.storeProduct);
 
-let upload = multer({storage : storage});
+router.get("/update/:id", productController.formUpdate);
+router.post("/update/:id", productController.updatePost);
 
-// Ruta para mostrar un auto
+router.get('/search', productController.search);
 
-router.get('/id/:id', productController.showProduct);
+router.post('/comentario/:id', productController.storeComentario);
 
-//Rutas para comentario
-
-router.post('/id/:id', productController.comments);
-
-// Rutas para eliminar un auto 
-router.post('/id/:id/delete', productController.deleteProduct);
-
-// Rutas para agregar un auto
-router.get('/add' , productController.showProductAdd);
-
-router.post('/store', upload.single('imgProduct'), productController.store);
-
-// Rutas para EDITAR una publicaion 
-
-router.get('/edit/:id' , productController.showProductEdit);
-
-router.post('/edit/:id', upload.single('imgProduct'), productController.updateProduct);
-
-
-
-
-
+router.post('/eliminar', productController.eliminarProducto);
 module.exports = router;
