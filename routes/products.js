c3ee89bee851ea4b4d2fbacd7bf2010ca1eb129e
unknown <chelmich@udesa.edu.ar>
2023-06-13 19:34:32 -0300

    j

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,7 @@ let path = require('path');
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/images/products')) 
+        cb(null, path.join(__dirname, '../public/images')) 
     },
     filename : function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
