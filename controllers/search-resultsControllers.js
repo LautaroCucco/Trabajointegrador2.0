598c03d6cc5452eaddd76292e06009b973832708
LautaroCucco <lcucco@udesa.edu.ar>
2023-06-15 22:50:02 -0300

    arreglo v3

--- controllers/search-resultsControllers.js
+++ controllers/search-resultsControllers.js
@@ -1,13 +1,29 @@
-var dataBase = require('../db/data')
+const db = require('../database/models/');
+const op = db.Sequelize.Op;
+const producto = db.Producto;
 
-var searchResultsControllers = {
-    showSearchResults: function(req, res){
-        return res.render('search-results',{
-            usuario: dataBase.usuario,
-            productos: dataBase.productos,
-            logueado: dataBase.usuario.logueado
-        })
+const searchResultsController = {
+    findProduct: (req, res) => {
+        let palabraBuscada = req.query.search;
+        let filtro ={
+            where :{
+             [op.or]: [
+               { model: { [op.like]: `%${ palabraBuscada}%` } },
+               { description: { [op.like]: `%${ palabraBuscada}%` } }
+             ]
+           }, 
+            include: [ { association: 'usuario' }],
+            order: [[ "createdAt" , "DESC"]]
+           }
+      
+        producto.findAll(filtro)
+        .then((result) => {
+        return res.render('search-results', { producto : result } )
+        }).catch((err) => {
+            console.log(err);
+        });
     }
+
 };
 
-module.exports = searchResultsControllers;
+module.exports = searchResultsController;
